d3a23bdfc696f0b500528b3b7e4c4e585fecd202
devTylr <tyler.ward1199@gmail.com>
2023-08-24 08:56:12 -0400

    Cleaned up code by putting in functions

--- script.js
+++ script.js
@@ -1,27 +1,19 @@
-const gridContainer = document.querySelector('.container');
+function changeGridSize(num = 16) {
+    const gridContainer = document.querySelector('.container');
+    gridContainer.style.gridTemplateColumns = `repeat(${num}, 1fr)`;
+    gridContainer.style.gridTemplateRows = `(repeat${num}, 1fr)`;
 
-const userInput = Number(prompt('Grid size?'));
-
-gridContainer.style.gridTemplateColumns = `repeat(${userInput}, 1fr)`;
-gridContainer.style.gridTemplateRows = `(repeat${userInput}, 1fr)`;
-
-for (let i = 0; i < userInput * userInput; i++) {
-    const gridBox = document.createElement('div');
-    gridBox.classList.add('box');
-    gridContainer.appendChild(gridBox);
+    for (let i = 0; i < num * num; i++) {
+        const gridBox = document.createElement('div');
+        gridBox.classList.add('box');
+        gridContainer.appendChild(gridBox);
+    }
 }
+changeGridSize();
 
 const boxes = document.querySelectorAll('.box');
 boxes.forEach(box => {
     box.addEventListener('mouseover', () => {
         box.style.backgroundColor = 'black';
     })
-})
-
-// NOTES
-
-// Get user input for grid size
-
-// Repeat that number for grid column and grid row
-
-// append that amount of grid boxes to grid container
+});
