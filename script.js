b0530cdb5ea12a0b25de7e4b315f80d83ecb8099
devTylr <tyler.ward1199@gmail.com>
2023-08-25 08:06:28 -0400

    Added button that changes grid size when under 120

--- script.js
+++ script.js
@@ -1,19 +1,43 @@
 function changeGridSize(num = 16) {
     const gridContainer = document.querySelector('.container');
+
     gridContainer.style.gridTemplateColumns = `repeat(${num}, 1fr)`;
-    gridContainer.style.gridTemplateRows = `(repeat${num}, 1fr)`;
+    gridContainer.style.gridTemplateRows = `repeat(${num}, 1fr)`;
 
     for (let i = 0; i < num * num; i++) {
         const gridBox = document.createElement('div');
         gridBox.classList.add('box');
         gridContainer.appendChild(gridBox);
     }
+
+    changeBackgroundColor();
 }
 changeGridSize();
 
-const boxes = document.querySelectorAll('.box');
-boxes.forEach(box => {
-    box.addEventListener('mouseover', () => {
-        box.style.backgroundColor = 'black';
-    })
+function changeBackgroundColor() {
+    const boxes = document.querySelectorAll('.box');
+    boxes.forEach(box => {
+            box.addEventListener('mouseover', () => {
+                box.style.backgroundColor = 'black';
+            })
+        });
+}
+
+function cleanGrid() {
+    const boxes = document.querySelectorAll('.box');
+    boxes.forEach(box => {
+            box.style.backgroundColor = 'white';
+        });
+}
+
+const changeButton = document.querySelector('.change-grid');
+changeButton.addEventListener('click', () => {
+    const userInput = Number(prompt('Prompt'));
+    
+    if(userInput > 120 || userInput === null) {
+        alert('ERROR: Please select a number less than 120.');
+    } else {
+        cleanGrid();
+        changeGridSize(userInput);
+    };
 });
